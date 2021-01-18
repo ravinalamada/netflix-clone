eda0b1133952d0350b936441f334a77d551476bc
Prisca Roseline <prisca.ros@onja.org>
2021-01-18 11:58:44 +0300

    I built netflix clone.

---
+++ src/components/jumbotron/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Jumbotron({ children, direction = 'row', ...restProps }) {
+  return (
+    <Item {...restProps}>
+      <Inner direction={direction}>{children}</Inner>
+    </Item>
+  )
+}
