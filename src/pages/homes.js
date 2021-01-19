b58de873c227ba7f1557cb7b5cda1708df0df5af
Prisca Roseline <prisca.ros@onja.org>
2021-01-19 11:59:41 +0300

    I built netflix clone landing page.

---
+++ src/pages/homes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import JumbotronContainer  from '../containers/jumbotron'
+
+
+function homes() {
+  return (
+      <JumbotronContainer />
+  )
+}
+
+export default homes
